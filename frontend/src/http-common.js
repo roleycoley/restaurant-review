ac78eb2e4ed19646294406645d6901c204a781a3
Cole Dungca <coledungca@coles-mbp.attlocal.net>
2023-01-20 23:06:38 -0800

    changed url

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default axios.create({
     // url of backend server (in server.js)
-    baseURL: "https://food-finder-p5j8.onrender.com",
+    baseURL: "https://food-finder-p5j8.onrender.com/api/v1",
     headers: {
         "Content-type": "application/json"
     }
